fda39d7e349119d9de17fd9207dc23e390347666
Omid Reza Vasheghani Farahani <omidrezav@gmail.com>
2021-06-16 14:58:37 +0430

    Added support for multiple FAQs for one object in jsonViewHelper

--- src/Services/Helpers/jsonViewHelper.php
+++ src/Services/Helpers/jsonViewHelper.php
@@ -8,24 +8,27 @@ use Modules\Faq\Models\Faq;
 
 class jsonViewHelper
 {
-    public static function render($faq)
+    public static function render( array $faqs )
     {
-        $faq = Faq::find($faq);
+        $faqs = Faq::whereIn('id', $faqs)->get();
         $faq_list = new \stdClass();
         $context = "@context";
         $faq_list->$context="https://schema.org";
         $type = "@type";
         $faq_list->$type = "FAQPage";
         $main_entity = [];
-        foreach( $faq->questions as $faq_question ){
-            $question = new \stdClass();
-            $question->$type = "Question";
-            $question->name = $faq_question->question;
-            $answer = new \stdClass();
-            $answer->$type = "Answer";
-            $answer->text = $faq_question->answer;
-            $question->acceptedAnswer = $answer;
-            $main_entity[] = $question;
+
+        foreach( $faqs as $faq ){
+            foreach( $faq->questions as $faq_question ){
+                $question = new \stdClass();
+                $question->$type = "Question";
+                $question->name = $faq_question->question;
+                $answer = new \stdClass();
+                $answer->$type = "Answer";
+                $answer->text = $faq_question->answer;
+                $question->acceptedAnswer = $answer;
+                $main_entity[] = $question;
+            }
         }
         $faq_list->mainEntity = $main_entity;
 
